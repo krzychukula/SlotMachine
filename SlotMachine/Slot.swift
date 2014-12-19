70a9c43ceab03e66515c85dbf657ac5bb5f7bf3a
krzychukula <krzychukula@gmail.com>
2014-12-19 15:28:37 +0100

    add slot struct

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Krzysztof Kula on 19/12/14.
+//  Copyright (c) 2014 Krzysztof Kula. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
